d0c40b59ca3bafda7f0e186923f609ed6fc0e643
tan9799 <tan90_0320@qq.com>
2026-05-16 16:29:58 +0800

    Java

---
+++ test8/src/Diamonds.java
@@ -0,0 +1,11 @@
+public class Diamonds {
+    final int weight = 10;//10克拉的钻石
+
+    public void bling(){
+        System.out.println("blingbling的");
+    }
+
+    public static void main(String[] args) {
+        Diamonds d = new Diamonds();
+    }
+}
---
+++ test8/src/PinkDiamonds.java
@@ -0,0 +1,5 @@
+public class PinkDiamonds extends Diamonds{
+    public void bling(){
+        System.out.println("粉钻也是blingbling的");
+    }
+}
