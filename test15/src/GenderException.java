0b5bf5884e82d674e8f4f3967085a7967a4aaaf8
tan9799 <tan90_0320@qq.com>
2026-05-19 13:49:59 +0800

    Java

---
+++ test15/src/GenderException.java
@@ -0,0 +1,5 @@
+public class GenderException extends Exception{ //自己定义的异常必须要继承Exception或者RuntimeException
+    public GenderException(String msg) {
+        super(msg);//调用父类的构造方法，Exception(msg)
+    }
+}
---
+++ test15/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    String name;
+    String gender;
+    public Person(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+}
