457b1cbc4ca829e8cde8047a808d335d10277fa5
tan9799 <tan90_0320@qq.com>
2026-06-26 20:38:54 +0800

    Java

---
+++ xun huan da yin shu ju/src/xun_huan_da_yin_shu_ju.java
@@ -0,0 +1,13 @@
+public class xun_huan_da_yin_shu_ju {
+    public static void main(String args[]) {
+        int sum = 0;
+        for(int i = 1; i <= 5; i++){
+            sum += i;
+            System.out.println(sum);
+        }
+        for(int i = 5; i >= 1; i--) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
