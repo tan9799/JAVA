1f185e31f3e1f0541f50ec2ce75996139048c9bc
tan9799 <tan90_0320@qq.com>
2026-07-11 19:24:06 +0800

    Java

---
+++ Exception/src/Exception.java
@@ -0,0 +1,10 @@
+public class Exception {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        try {
+            System.out.println(arr[10]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("数组越界");
+        }
+    }
+}
