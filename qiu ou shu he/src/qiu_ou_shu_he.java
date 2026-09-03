c5aff729a6ee328eceff95fb17e52df9e77a9aa6
userName <tan90_0320@qq.com>
2026-09-03 16:37:17 +0800

    Java

---
+++ qiu ou shu he/src/qiu_ou_shu_he.java
@@ -0,0 +1,11 @@
+public class qiu_ou_shu_he {
+    public static void main(String args[]){
+        int sum = 0;
+        for(int i = 1; i <= 100; i++){
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
