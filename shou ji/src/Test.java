447c0ae8ed1f8c788cefe4627e8ba81e2e99dc7d
userName <tan90_0320@qq.com>
2026-09-13 10:06:26 +0800

    Java

---
+++ shou ji/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        shou_ji s = new shou_ji();
+        s.price = 6999;
+        s.color = "白色";
+        s.pinpai = "华为";
+    }
+}
---
+++ shou ji/src/shou_ji.java
@@ -0,0 +1,5 @@
+public class shou_ji {
+    String pinpai;
+    String color;
+    int price;
+}
